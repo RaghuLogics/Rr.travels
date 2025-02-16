80eec25281133a1c188f2268732d79f81a5699b6
RaghuLogics <chundruraghuram@gmail.com>
2025-02-17 00:38:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//const pathName = window.location.pathname;
+//alert(pathName)
+
+//secondmenu hover active effect:-
+//const activePage = window.location.pathname;
+//consr navlinks = document.querySelectorAll('.secondmenu a'). 
+//forEach(link => {
+ //   if(link.href.includes(`${activePage}`)){
+  //      link.classList.add(`active`);
+        
+  // }
+//})
+
